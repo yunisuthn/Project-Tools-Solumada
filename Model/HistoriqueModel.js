2e95f7e977a1650c2a97ba621ffd99d0487c688e
yunisuthn <yunis.thn@gmail.com>
2022-11-18 14:43:47 +0300

    update delete

--- Model/HistoriqueModel.js
+++ Model/HistoriqueModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const historique = mongoose.Schema({
     user: String,
     model: String,
+    crud: String,
     date: Date,
     old: Object,
     new: Object
